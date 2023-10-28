e0b8a38535f418827565a9b5f2f23af974aa2441
DSunilDev <sunilkumardeveops@gmail.com>
2023-10-28 18:50:33 +0530

    Initial

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const multer = require('multer');
 
 const app=express();
 
+const db=require('./DATABASE/database');
+
 app.set('views',path.join(__dirname,'views'));
 app.set('view engine','ejs');
 
@@ -14,6 +16,21 @@ app.use(express.static('fonts'));
 app.use(express.static('js'));
 app.use(express.static('img'));
 app.use(express.static('images'));
+
+const storageconfig=multer.diskStorage({
+    destination:function(req,file,cb){
+        cb(null,'uploads') },
+        filename:function(req,file,cb)
+        {
+            cb(null,Date.now()+'-'+file.originalname);
+ }
+});
+
+const upload=multer({storage:storageconfig});
+app.use('/uploads',express.static('uploads'))
+
+
+
 
 app.get('/',function(req,res)
 {
@@ -39,9 +56,40 @@ app.get('/Gallery',function(req,res){
     res.render('gallery')
 })
 
-app.get('/Post',function(req,res)
-{
-    res.render('posts')
+
+app.get('/AddPost',function(req,res){
+    res.render('addpost')
 })
 
-app.listen(500)
+app.get('/Post',async function(req,res)
+{
+    const postdata=await db.getDb().collection('post').find().toArray();
+    res.render('post',{posts:postdata})
+})
+
+
+app.post('/posts', upload.single('image'), async function (req, res) {
+    const { title, content,date,venue } = req.body;
+    const image = req.file;
+    
+    const postdata = {
+        title: title,
+        content: content,
+        date:date,
+        venue:venue,
+        imagePath: image.path
+    };
+
+    // Assuming you are using a MongoDB database
+    
+        await db.getDb().collection('post').insertOne(postdata);
+        res.redirect('/Addpost');
+});
+
+
+module.exports=app;
+
+db.connectToDatabase().then(function () {
+    app.listen(500);
+  });
+  
