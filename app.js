1df8993ba4bfb1fd347a536cb215cbc2549eaf5e
DSunilDev <sunilkumardeveops@gmail.com>
2023-10-29 10:54:44 +0530

    Declaration error

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express=require('express')
 const path=require('path')
 const multer = require('multer');
-const bcry=require('bcryptjs')
+const bcrypt=require('bcryptjs')
 const bodyParser = require('body-parser');
 
 const app=express();
