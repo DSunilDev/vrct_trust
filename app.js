38c126a3f1eaf43f9ea306973993e86fd66c0ac3
DSunilDev <sunilkumardeveops@gmail.com>
2023-10-29 08:25:56 +0530

    jj

--- app.js
+++ app.js
@@ -59,9 +59,9 @@ app.get('/signup',function(req,res){
 app.post('/signup',async function(req,res)
 {
     const userdata=req.body;
-    const email=userdata.mail;
+    const email=userdata.gmail;
     const password=userdata.password;
-    const existingdata=await db.getDb().collection('users').findOne({mail:email});
+    const existingdata=await db.getDb().collection('users').findOne({email:email});
 
     if(existingdata)
     {
@@ -71,7 +71,7 @@ app.post('/signup',async function(req,res)
     const hashedpassword=await bcry.hash(password,12);
 
     const users={
-        mail:email,
+        email:email,
         password:hashedpassword,
     };
 
