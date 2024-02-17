bbe22fccd7575291ad9db8ac56136bfcceba7b31
DSunilDev <sunilkumardeveops@gmail.com>
2024-02-17 09:33:35 +0530

    Search Engine Optimised

--- app.js
+++ app.js
@@ -283,6 +283,21 @@ app.post('/service', async function (req, res) {
         res.redirect('/Success');
 });
 
+app.get('/sitemap', function(req, res) {
+    // Assuming 'sitemap.xml' is in the root directory of your project
+    const filePath = __dirname + '/sitemap.xml';
+    
+    fs.readFile(filePath, 'utf8', function(err, data) {
+        if (err) {
+            console.error('Error reading sitemap.xml:', err);
+            res.status(500).send('Internal Server Error');
+        } else {
+            res.set('Content-Type', 'application/xml');
+            res.send(data);
+        }
+    });
+});
+
 
 app.use(function(req,res)
 {
